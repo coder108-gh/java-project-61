9307794c9eabc0c8b036f5a3bfaaa84542c4ed15
Eugene Kuzmin <eugene_kuzmin@mail.ru>
2023-05-03 17:54:30 +0300

    fourth step

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,11 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void Logon() {
+    public static void logon() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("May I have your name? ");
         String userName = scanner.next();
-        System.out.println(String.format("Hello, %s",userName));
+        System.out.println(String.format("Hello, %s", userName));
         scanner.close();
     }
 }
